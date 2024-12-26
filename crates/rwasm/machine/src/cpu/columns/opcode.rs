b167d7ee68fdf1f6de916a2aa065861903eceefe
wangyao <nagatoismdrama@gmail.com>
2024-12-26 16:26:44 +0800

    add opcode

--- crates/rwasm/machine/src/cpu/columns/opcode.rs
+++ crates/rwasm/machine/src/cpu/columns/opcode.rs
@@ -1,7 +1,6 @@
 use p3_field::PrimeField;
-use rwasm::rwasm::InstructionExtra;
+use rwasm::{engine::bytecode::Instruction, rwasm::InstructionExtra};
 use sp1_derive::AlignedBorrow;
-use sp1_rwasm_executor::{Instruction, Opcode};
 use std::{
     mem::{size_of, transmute},
     vec::IntoIter,
@@ -26,28 +25,54 @@ const fn make_selectors_col_map() -> OpcodeSelectorCols<usize> {
 pub struct OpcodeSelectorCols<T> {
     /// Table selectors for opcodes.
     pub is_alu: T,
-
+    pub is_ordinary_alu: T,
+    pub is_comparison_alu:T,
     /// Table selectors for opcodes.
     pub is_ecall: T,
 
     pub is_auipc: T,
     pub is_unimpl: T,
+    // The following does not need selector because 
+    // they are check directly with sp1 alu.
+    // pub is_i32add: T,
+    // pub is_i32sub: T,
+    // pub is_i32mul: T,
+    // pub is_i32divu:T,
+    // pub is_i32divs:T,
+    // pub is_i32remu: T,
+    // pub is_i32rem: T,
 
-    pub is_iadd32: T,
-    pub is_isub32: T,
-    pub is_imul32: T,
-    pub is_idivu32:T,
-    pub is_idivs32:T,
-    pub is_iremu32: T,
-    pub is_irem32: T,
-
-
+    // pub is_i32lts: T,
+    // pub is_i32ltu: T,
+    pub is_i32les: T,
+    pub is_i32leu: T,
+    pub is_i32gts: T,
+    pub is_i32gtu: T,
+    pub is_i32ges: T,
+    pub is_i32geu: T,
+    pub is_i32eq: T,
+    pub is_i32ne: T,
+    pub is_i32eqz: T,
 }
 
 impl<F: PrimeField> OpcodeSelectorCols<F> {
     pub fn populate(&mut self, instruction: Instruction) {
         let op_code = instruction.code_value() as u32;
         if instruction.is_alu_instruction() {
+            // I32 Lts and I32 Ltu can use sp1 circuit directly so 
+            // they do not need go to compare
+            match instruction {
+                Instruction::I32GtS| Instruction::I32GtU |
+                Instruction::I32GeS | Instruction::I32GeU|
+                Instruction::I32LeS |Instruction::I32LeU|
+                Instruction::I32Eqz |Instruction::I32Eq |
+                Instruction::I32Ne=>{
+                    self.is_comparison_alu = F::one();
+                }
+                _=>{
+                    self.is_ordinary_alu = F::one();
+                }
+            }
             self.is_alu = F::one();
         } else if instruction.is_ecall_instruction() {
             self.is_ecall = F::one();
@@ -55,6 +80,21 @@ impl<F: PrimeField> OpcodeSelectorCols<F> {
             todo!()
         } else if instruction.is_branch_instruction() {
             todo!()
+        }
+        match instruction{
+            Instruction::I32Eqz => { self.is_i32eqz = F::one();},
+            Instruction::I32Eq => {self.is_i32eq=F::one()}
+            Instruction::I32Ne => {self.is_i32ne=F::one()},
+            Instruction::I32GtS => {self.is_i32gts=F::one()},
+            Instruction::I32GtU => {self.is_i32gtu=F::one()},
+            Instruction::I32LeS => {self.is_i32les=F::one()},
+            Instruction::I32LeU => {self.is_i32leu=F::one()},
+            Instruction::I32GeS => {self.is_i32ges=F::one()},
+            Instruction::I32GeU => {self.is_i32geu=F::one()},
+            Instruction::I64Eqz => {self.is_i32eqz=F::one()},
+            Instruction::I64Eq => {self.is_i32eq=F::one()},
+            Instruction::I64Ne => {self.is_i32ne=F::one()},
+           _=>{}
         }
 
         
@@ -66,9 +106,11 @@ impl<T> IntoIterator for OpcodeSelectorCols<T> {
     type IntoIter = IntoIter<T>;
 
     fn into_iter(self) -> Self::IntoIter {
-        let columns = vec![self.is_alu, self.is_ecall, self.is_auipc, self.is_unimpl,self.is_iadd32,self.is_isub32,
-        self.is_imul32,self.is_idivs32,self.is_idivu32,self.is_irem32,self.is_iremu32];
-        assert_eq!(columns.len(), NUM_OPCODE_SELECTOR_COLS);
+        let columns = vec![self.is_alu, self.is_ordinary_alu,self.is_comparison_alu,self.is_ecall, self.is_auipc, self.is_unimpl,
+        self.is_i32les,self.is_i32leu,
+        self.is_i32ges,self.is_i32geu,self.is_i32gts,self.is_i32gtu,
+        self.is_i32eq,self.is_i32ne,self.is_i32eqz];
+        assert_eq!(columns.len(), NUM_OPCODE_SELECTOR_COLS); 
         columns.into_iter()
     }
 }
